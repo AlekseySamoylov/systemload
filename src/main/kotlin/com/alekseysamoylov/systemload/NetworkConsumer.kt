796c3dd303a46a3c4a0aaef4e5020ed615e1d9a1
alekseysamoylov <alekseysamoylov89@gmail.com>
2020-06-07 08:06:02 +0400

    add server

--- src/main/kotlin/com/alekseysamoylov/systemload/NetworkConsumer.kt
+++ src/main/kotlin/com/alekseysamoylov/systemload/NetworkConsumer.kt
@@ -2,7 +2,8 @@ package com.alekseysamoylov.systemload
 
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
-import java.io.OutputStream
+import java.io.DataInputStream
+import java.net.ServerSocket
 import java.net.Socket
 import java.util.concurrent.TimeUnit
 import javax.annotation.PreDestroy
@@ -14,9 +15,23 @@ class NetworkConsumer {
 
   private val log = LoggerFactory.getLogger(this::class.java)
   private val workers = mutableListOf<Worker>()
+  private var serverSocketWorker: Worker? = null
+  private var serverSocket: ServerSocket? = null
   fun consume(byteArraySizeToWrite: Int, ipAddress: String, port: Int) {
     stopWorkers()
-    workers.clear()
+    serverSocket?.close()
+    serverSocket = ServerSocket(port)
+    val ssocket: Socket = serverSocket!!.accept()
+    serverSocketWorker = Worker {
+      try {
+        val dis = DataInputStream(ssocket.getInputStream())
+        val bytes = dis.readAllBytes()
+        log.info("Bytes read {}", bytes.size)
+      } catch (ex: Exception) {
+        log.warn("Network err", ex)
+      }
+    }
+    Thread(serverSocketWorker).start()
     val arrayToWrite = Random.nextBytes(ByteArray(byteArraySizeToWrite))
     val worker = Worker {
       Socket(ipAddress, port).use { socket ->
@@ -33,6 +48,9 @@ class NetworkConsumer {
   @PreDestroy
   fun teardown() {
     stopWorkers()
+    workers.clear()
+    serverSocketWorker?.stop()
+    serverSocketWorker = null
   }
 
   fun size(): Int = workers.size
